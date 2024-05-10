06d8bf2cbc9fe9207a181b42ebd5d4fdd5f9ab9c
Juan Jose Suarez <suarezorregojuanjosegmail.com>
2024-05-09 20:02:25 -0500

    Proyecto formativo

---
+++ src/app/features/salud-gallinas/salud-gallinas.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SaludGallinasRoutingModule } from './salud-gallinas-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SaludGallinasRoutingModule
+  ]
+})
+export class SaludGallinasModule { }
